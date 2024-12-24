bc04961478cc87f9a0e91e6c407bd321e6943d4b
m4milaad <mb4milad.bhattt@gmail.com>
2024-12-24 22:42:03 +0530

    changes in cc.c

--- cc.c
+++ cc.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    // your code goes here
-
-   for (size_t i = 0; i < 10000000000; i++)
-   {
-    printf("%d",i);
-   }
-   
+	// your code goes here
+    int a,b,c,d,t;
+    scanf("%d",&t);
+    
+    while(t--)
+    {
+        scanf("%d %d %d %d",&a,&b,&c,&d);
+        int f,s;
+        f=(a>b)?a:b;
+        s=(c>d)?c:d;
+        printf("%d\n",f+s);
+        
+    }
+    
 }
+
