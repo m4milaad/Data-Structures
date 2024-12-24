377ac9ecfdcd2dd68dadf80937534dfd7ffe02c9
m4milaad <mb4milad.bhattt@gmail.com>
2024-12-24 14:48:56 +0530

    changes to cc.c

--- cc.c
+++ cc.c
@@ -2,25 +2,10 @@
 
 int main() {
     // your code goes here
-    int n, t;
-    scanf("%d", & t);
-    while (t--)
-    {
-        scanf("%d", & n);
 
-        if (n < 16)
-        {
-            if (n < 10 && n % 2 != 0)
-                printf("Lower Double\n");
-            else printf("Lower Single\n");
-        }
-        else
-        {
-            if(n<25&&n%2==0)
-            printf("Upper Double\n");
-            else printf("Lower Single\n");
-        }
-
-    }
-
+   for (size_t i = 0; i < 10000000000; i++)
+   {
+    
+   }
+   
 }
