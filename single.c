08ad1b405604380193f0371b1115cd4c55eb1f0e
m4milaad <mb4milad.bhattt@gmail.com>
2025-01-13 01:43:51 +0530

    updated single linked list

--- single.c
+++ single.c
@@ -1,292 +1,399 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
+typedef struct sll
 {
     int data;
-    struct node *next;
-};
-struct node *head = NULL;
-void create();
-void printlist();
-void insert_node();
-void del_node();
-void reverse();
-void freelist();
-void sort_list();
-void concat_list();
+    struct sll *next;
+} Node;
+
+// functions
+void create_list(Node **head);
+void insert_node(Node **head);
+void delete_node(Node **head);
+void modify_node(Node *head);
+void reverse_list(Node **head);
+void sort_list(Node *head);
+void concat_list(Node **head);
+void free_list(Node **head, int a);
+void print_list(Node *head);
+int list_size(Node *head);
+// loop resolve function to be added( 1->2->3->4->5->3 )
 
 int main()
 {
-    int check;
-
+    printf("initializing List...\n");
+    Node *head = NULL;
+    create_list(&head);
+    int choice;
     do
     {
-        printf("\n\nWhat do you want to do\n\n");
-        printf("Press \"1\" to create list\n");
-        printf("Press \"2\" to insert a node in the list\n");
-        printf("Press \"3\" to delete a nodes in the list\n");
-        printf("Press \"4\" to reverse the list\n");
-        printf("Press \"5\" to print the list\n");
-        printf("Press \"6\" to free the list\n");
-        printf("Press \"7\" to sort the list\n");
 
-        printf("Press \"0\" to end\n");
+        printf("\"1\" to insert node\n");
+        printf("\"2\" to delete node\n");
+        printf("\"3\" to modify node\n");
+        printf("\"4\" to reverse list\n");
+        printf("\"5\" to sort list\n");
+        printf("\"6\" to merge 2 lists\n");
+        printf("\"7\" to print list\n");
+        printf("\"8\" to List size\n");
+        printf("\"9\" to free list\n");
+        printf("\"0\" to exit\n");
+        printf("\n\tEnter choice: ");
+        scanf("%d", &choice);
 
-        printf("\nEnter the value of opertion to execute: ");
-        scanf("%d", &check);
-
-        switch (check)
+        switch (choice)
         {
-        case 0:
-            printf("\nThank you for using this\n");
-            freelist();
-            break;
         case 1:
-            create();
+            insert_node(&head);
             break;
         case 2:
-            insert_node();
+            delete_node(&head);
             break;
         case 3:
-            del_node();
+            modify_node(head);
             break;
         case 4:
-            reverse();
+            reverse_list(&head);
             break;
         case 5:
-            printlist();
+            sort_list(head);
             break;
         case 6:
-            freelist();
+            concat_list(&head);
             break;
         case 7:
-            sort_list();
+            print_list(head);
+            break;
+        case 8:
+            printf("List size is: %d\n", list_size(head));
+            break;
+        case 9:
+            free_list(&head, 1);
+            break;
+        case 0:
+            printf("Freeing nodes...\n");
+            free_list(&head, 0);
+            printf("Exiting...\n");
             break;
         default:
-            printf("\n\tOperation not valid\n");
-            printf("\nEnter the value of opertion to execute\n");
-            printf("\n or Press \"0\" to end: ");
-            scanf("%d", &check);
+            printf("Invalid option\nTry again!\n");
             break;
         }
-    } while (check != 0);
+
+    } while (choice != 0);
+
     return 0;
 }
 
-void create()
+void create_list(Node **head)
 {
-    if (head != NULL)
-    {
-        printf("List is already created\n");
-        printf("You can now add or delete nodes: ");
-    }
-
     int no;
-    printf("Enter the number of nodes you want to create: ");
+    printf("Enter the number of nodes: ");
     scanf("%d", &no);
-
-    if (no <= 0)
-    {
-        printf("Invalid input. Number of nodes should be a positive integer.\n");
-        return;
-    }
-
-    head = (struct node *)malloc(sizeof(struct node));
-    struct node *current = head;
+    Node *current = NULL;
 
     for (int i = 1; i <= no; i++)
     {
-        printf("Enter the data in node %d: ", i);
-        scanf("%d", &current->data);
-
-        if (i < no)
+        if ((*head) == NULL)
         {
-            current->next = (struct node *)malloc(sizeof(struct node));
-            current = current->next;
+            (*head) = (Node *)malloc(sizeof(Node));
+            current = (*head);
         }
         else
-            current->next = NULL;
+        {
+            current->next = (Node *)malloc(sizeof(Node));
+            current = current->next;
+        }
+
+        printf("Enter the data in node %d: ", i);
+        scanf("%d", &current->data);
+        current->next = NULL;
     }
+
+    printf("List created sucessfully\n");
 }
-void printlist()
+
+void insert_node(Node **head)
 {
-    if (head == NULL)
-    {
-        printf("\n\tNo nodes created.\n");
-        return;
-    }
+    int pos;
+    int size = list_size(*head);
+    print_list(*head);
 
-    struct node *temp = head;
-    printf("The linked list is: ");
-
-    while (temp != NULL)
-    {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-
-    printf("\n");
-}
-void insert_node()
-{
-    if (head == NULL)
-    {
-        printf("No nodes created\nPlease create the Linked List first");
-        return;
-    }
-
-    struct node *new_node, *current = head;
-    int pos, count = 0;
-
-    current = head;
-    while (current != NULL)
-    {
-        current = current->next;
-        count++;
-    }
-    current = head;
-    new_node = (struct node *)malloc(sizeof(struct node));
-    printf("Enter the position you want to add node at (1-%d): ", count);
+    printf("Enter the position where you want to add node (1-%d): ", size + 1);
     scanf("%d", &pos);
+    Node *new = NULL;
+    new = (Node *)malloc(sizeof(Node));
+    if (new == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        return;
+    }
 
-    printf("Enetr the data you want to add in new node: ");
-    scanf("%d", &new_node->data);
-    new_node->next = NULL;
+    new->next = NULL;
+    printf("Enter data in node: ");
+    scanf("%d", &new->data);
 
     if (pos == 1)
     {
-        new_node->next = head;
-        head = new_node;
+        new->next = (*head);
+        (*head) = new;
     }
-    else if (pos > 1 && pos <= count)
+    else if (pos == size + 1)
     {
+        Node *current = NULL;
+        for (current = (*head); current->next != NULL; current = current->next)
+            ;
+        current->next = new;
+    }
+    else if (pos > 1 && pos <= size)
+    {
+        Node *current = NULL;
+        current = (*head);
         for (size_t i = 1; i < pos - 1; i++)
         {
             current = current->next;
         }
-
-        new_node->next = current->next;
-        current->next = new_node;
-    }
-    else if (pos == count + 1)
-    {
-
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = new_node;
+        new->next = current->next;
+        current->next = new;
     }
     else
-        printf("Position does not exist\n");
-}
-void del_node()
-{
-
-    if (head == NULL)
     {
-        printf("No nodes available to delete\n");
+        printf("Position doesn't exist\n");
         return;
     }
 
-    struct node *previous, *current = head;
-    int pos, count = 0, psave;
+    printf("Node inserted sucessfully\n");
+}
 
-    while (current != NULL)
+void delete_node(Node **head)
+{
+    if ((*head) == NULL)
     {
-        current = current->next;
-        count++;
+        printf("Linked list is already Empty\n");
+        return;
     }
 
-    printf("Enter the node you want to delete(1 - %d): ", count);
+    int pos;
+    int size = list_size(*head);
+    print_list(*head);
+
+    printf("Enter the pos of node to delete: ");
     scanf("%d", &pos);
 
-    psave = pos - 1;
     if (pos == 1)
     {
-        current = head;
-        head = head->next;
+        Node *current = (*head);
+        (*head) = current->next;
         free(current);
     }
-    else if (pos > 1 && pos < count)
+    else if (pos == size)
     {
-        for (current = head; pos != 1; pos--)
-        {
-            current = current->next;
-        }
-        for (previous = head; psave != 1; psave--)
-        {
-            previous = previous->next;
-        }
-
-        previous->next = current->next;
-        free(current);
-    }
-    else if (pos == count)
-    {
-        current = head;
-        while (current->next->next != NULL)
-        {
-            current = current->next;
-        }
-
+        Node *current = (*head);
+        for (; current->next->next != NULL; current = current->next)
+            ;
         free(current->next);
         current->next = NULL;
     }
+    else if (pos > 1 && pos < size)
+    {
+        Node *current = (*head);
+        for (size_t i = 1; i < pos - 1; i++)
+        {
+            current = current->next;
+        }
+        Node *temp = current->next;
+        current->next = temp->next;
+        free(temp);
+    }
     else
-        printf("Invalid position\n");
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    printf("Node deleted sucessfully\n");
 }
-void reverse()
+
+void modify_node(Node *head)
 {
-    struct node *current = head, *prev = NULL, *next = NULL;
+    int find, replace, found = 0;
+    print_list(head);
+    printf("Enter value to find: ");
+    scanf("%d", &find);
+
+    printf("Enter the value to replace with: ");
+    scanf("%d", &replace);
+
+    Node *current = head;
+    for (; current != NULL; current = current->next)
+    {
+        if (current->data == find)
+        {
+            current->data = replace;
+            found = 1;
+            break;
+        }
+    }
+    if (!found)
+    {
+        printf("Element not found!\n");
+        return;
+    }
+}
+
+void print_list(Node *head)
+{
+    if (head == NULL)
+    {
+        printf("List is Empty\n");
+        return;
+    }
+
+    printf("\tThe list is: ");
+    Node *current = head;
+    for (; current != NULL; current = current->next)
+    {
+        printf("%d ", current->data);
+    }
+    printf("\n");
+    printf("\n");
+}
+
+int list_size(Node *head)
+{
+    int count = 0;
+    Node *current = head;
     while (current != NULL)
     {
-        next = current->next;
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+void reverse_list(Node **head)
+{
+    Node *current, *prev, *upcoming;
+    current = prev = upcoming = NULL;
+    for (current = (*head); current != NULL; current = upcoming)
+    {
+        upcoming = current->next;
         current->next = prev;
         prev = current;
-        current = next;
     }
-    head = prev;
+    (*head) = prev;
+    printf("\t\t\tList reversed\n");
+    print_list(*head);
 }
-void freelist()
+
+void sort_list(Node *head)
 {
-    struct node *current;
-    while (head != NULL)
+    int flag;
+    printf("1. Accending order\n2. Decending order\nHow do you want to sort the list: ");
+    scanf("%d", &flag);
+
+    if (flag == 1 || flag == 2)
     {
-        current = head;
-        head = head->next;
-        free(current);
-    }
-}
-void sort_list()
-{
-    int count, swap;
-    struct node *current, *current2;
-    for (current = head, count = 0; current != NULL; current = current->next, count++)
-        ;
-    for (current = head; current->next != NULL; current = current->next)
-    {
-        for (current2 = current->next; current2 != NULL; current2 = current2->next)
+        if (flag == 1)
         {
-            if (current->data > current2->data)
+            Node *current, *current2;
+            current2 = current->next;
+            for (current = head; current->next != NULL; current = current->next)
             {
-                swap = current->data;
-                current->data = current2->data;
-                current2->data = swap;
+                for (current2 = current->next; current2 != NULL; current2 = current2->next)
+                {
+                    if (current->data > current2->data)
+                    {
+                        int swap = current->data;
+                        current->data = current2->data;
+                        current2->data = swap;
+                    }
+                }
+            }
+        }
+        else
+        {
+            Node *current, *current2;
+            current2 = current->next;
+            for (current = head; current->next != NULL; current = current->next)
+            {
+                for (current2 = current->next; current2 != NULL; current2 = current2->next)
+                {
+                    if (current->data < current2->data)
+                    {
+                        int swap = current->data;
+                        current->data = current2->data;
+                        current2->data = swap;
+                    }
+                }
             }
         }
     }
-    printf("The linked list is sorted\n");
-}
-void  concat_list()
-{
-    if (head==NULL)
+    else
     {
-        printf("No previous list created");
-        printf("Create a list first to concat");
+        printf("Wrong input!\n");
         return;
     }
-    
-    
+}
+
+void concat_list(Node **head)
+{
+    Node *current = NULL, *head2 = NULL;
+    int flag;
+    printf("1. Current list.\n2. New List\nWhich list do you want at first: ");
+    scanf("%d", &flag);
+
+    if (flag == 1 || flag == 2)
+    {
+        if (flag == 1)
+        {
+            printf("Enter data for new list\n");
+            create_list(&head2);
+            for (current = (*head); current->next != NULL; current = current->next)
+                ;
+            current->next = head2;
+        }
+        else
+        {
+            printf("Enter data for new list\n");
+            create_list(&head2);
+            for (current = head2; current->next != NULL; current = current->next)
+                ;
+            current->next = (*head);
+            (*head) = head2;
+        }
+    }
+    else
+    {
+        printf("Invalid input\n");
+        return;
+    }
+}
+
+void free_list(Node **head, int a)
+{
+    Node *current = NULL;
+    while ((*head) != NULL)
+    {
+        current = (*head);
+        (*head) = (*head)->next;
+        free(current);
+    }
+
+    if (!a)
+        return;
+
+    char dec;
+    printf("List freed\n");
+    printf("Do you want to create a new list(y/n): ");
+    scanf(" %c", &dec);
+
+    if (dec == 'y' || dec == 'Y')
+        create_list(head);
+    else
+    {
+        printf("Exiting...\n");
+        exit(1);
+    }
 }
